18d433f8ca2ff3b5f96a4f255f90b15be585ecc2
Guilherme <guilhermepolicarpio@gmail.com>
2023-03-03 10:41:38 -0300

    feat: refactoring top  page

--- src/components/FlashcardScreen.js
+++ src/components/FlashcardScreen.js
@@ -1,56 +1,72 @@
-import React from 'react'
-import Top from './top'
+import React from 'react';
+import styled from 'styled-components';
+
+import Correct from '../../src/assets/images/correct.png';
+import Doubt from '../../src/assets/images/doubt.png';
+import Error from '../../src/assets/images/error.png';
 import Cards from './Cards';
 import Footer from './Footer';
-import Error from "../../src/assets/images/error.png"
-import Doubt from "../../src/assets/images/doubt.png"
-import Correct from "../../src/assets/images/correct.png"
+import Top from './top';
 
-    const questions = [
-        {question: " O que é JSX? ", answer:"Uma extensão de linguagem do JavaScript"},
-        {question: "O React é __", answer:"Uma biblioteca JavaScript para construção de interfaces"},
-        {question: "Componentes devem iniciar com __", answer:"letra maiúscula"},
-        {question: " Podemos colocar __ dentro do JSX", answer:"expressões"},
-        {question: "O ReactDOM nos ajuda __", answer:"interagindo com a DOM para colocar componentes React na mesma"},
-        {question: "Usamos o npm para __", answer:"gerenciar os pacotes necessários e suas dependências"},
-        {question: "Usamos props para __", answer:"passar diferentes informações para componentes "},
-        {question: "Usamos estado (state) para __", answer:"dizer para o React quais informações quando atualizadas devem renderizar a tela novamente"}
-    ];
+const questions = [
+    { question: " O que é JSX? ", answer: "Uma extensão de linguagem do JavaScript" },
+    { question: "O React é __", answer: "Uma biblioteca JavaScript para construção de interfaces" },
+    { question: "Componentes devem iniciar com __", answer: "letra maiúscula" },
+    { question: " Podemos colocar __ dentro do JSX", answer: "expressões" },
+    { question: "O ReactDOM nos ajuda __", answer: "interagindo com a DOM para colocar componentes React na mesma" },
+    { question: "Usamos o npm para __", answer: "gerenciar os pacotes necessários e suas dependências" },
+    { question: "Usamos props para __", answer: "passar diferentes informações para componentes " },
+    { question: "Usamos estado (state) para __", answer: "dizer para o React quais informações quando atualizadas devem renderizar a tela novamente" }
+];
 
-    function alternate() { 
-        return Math.random() - 0.5; 
+function alternate() {
+    return Math.random() - 0.5;
+}
+
+questions.sort(alternate);
+
+export default function FlashcardScreen() {
+
+    const [result, setResult] = React.useState([]);
+
+    function updateResult(answer) {
+        setResult([...result, answer]);
     }
-    
-    questions.sort(alternate);
 
-    export default function FlashcardScreen(){
-
-        const [result, setResult] = React.useState([]);
-
-        function updateResult(answer){
-            setResult([...result,answer]);
-            console.log(result)
+    function optionIcon(recall) {
+        if (recall === 'Error') {
+            return Error;
+        } else if (recall === 'Doubt') {
+            return Doubt;
+        } else if (recall === 'Correct') {
+            return Correct;
         }
+    }
+    return (
+        <ContentWrapper>
+            <Top />
+            <>{
+                questions.map((flashCard, index) => {
+                   return <Cards
+                        key={index}
+                        questionNumber={index + 1}
+                        question={flashCard.question}
+                        answer={flashCard.answer}
+                        updateResult={updateResult}
+                        optionIcon={optionIcon} />
+                })}
+            </>
+            <Footer result={result} optionIcon={optionIcon} questions={questions} />
 
-        function optionIcon(recall){
-            if(recall==='Error') {
-                return Error;
-            } else if(recall==='Doubt') {
-                return Doubt;
-            } else if(recall==='Correct') {
-                return Correct;
-            }
-        }
-    return(
-    <div className="content flashcard-screen">
-        <Top/>
-        <div>{
-            questions.map((flashCard,index) => { 
-                return <Cards key={index} questionNumber={index+1} question={flashCard.question} answer={flashCard.answer} updateResult={updateResult} optionIcon={optionIcon}/>
-            })}
-        </div>
-        <Footer result={result} optionIcon={optionIcon} questions={questions}/>
-     
-    </div>
+        </ContentWrapper>
     )
- }
+}
+
+const ContentWrapper = styled.section`
+  display: flex;
+  justify-content: flex-start;
+  flex-direction: column;
+  align-items: center;
+  z-index: 1;
+  background-color: rgba(251, 107, 107, 1);
+`;
--- src/components/top.js
+++ src/components/top.js
@@ -1,10 +1,38 @@
-import Logo from "../../src/assets/images/logo-pequeno.png"
+import styled from 'styled-components';
 
-export default function Top(){
+import Logo from '../../src/assets/images/logo-pequeno.png';
+
+export default function Top() {
     return (
-        <div className="top">
-              <img src={Logo} alt=""/>
-              <h1>ZapRecall</h1>
-            </div>
+        <TopWrapper>
+            <img src={Logo} alt="" />
+            <h1>ZapRecall</h1>
+        </TopWrapper>
     )
 }
+
+const TopWrapper = styled.section`
+display: flex;
+justify-content: center;
+align-items: center;
+margin: 2.2em;
+padding: 0.5em;
+
+img{
+width: 52px;
+height: 50px;
+margin-right:10px;
+}
+
+h1{
+font-family: 'Righteous';
+  font-style: normal;
+  font-weight: bold;
+  font-size: 36px;
+  padding: 0.5em 0em;
+  color: #FFFFFF;
+  display: flex;
+  justify-content: center;
+  align-items:center;
+}
+`;
